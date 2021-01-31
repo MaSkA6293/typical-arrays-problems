e20b0f48b16f4b431d7d7eadb5c386ef2f7ca5c8
MaSkA6293 <MaSkA6293@yandex.ru>
2021-01-31 13:53:20 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,48 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (checkArray(array)) {
+    let min = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (min > array[i]) {
+        min = array[i]
+      }
+    }
+    return min;
+  } else {
+    return 0
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (checkArray(array)) {
+    let max = array[0]
+    for (let i = 1; i < array.length; i++) {
+      if (max < array[i]) {
+        max = array[i]
+      }
+    }
+    return max;
+  } else {
+    return 0
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (checkArray(array)) {
+    return array.reduce((acc, curr) => {
+      return acc = acc + curr
+    }, 0) / array.length
+  } else {
+    return 0
+  }
+}
+
+function checkArray(array) {
+  if (!Array.isArray(array)) {
+    return false
+  }
+  if (array.length === 0) {
+    return false
+  }
+  return true
 }
